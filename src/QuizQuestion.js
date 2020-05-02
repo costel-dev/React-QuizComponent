26524e8b16d98ee61b8d1ed61c991ca05d0913c8
costel-dev <costelcuconoiu@gmail.com>
2020-05-02 21:55:22 +0200

    added state to display a message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,21 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnswer: false };
+  }
+
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+      this.setState(() => {
+        return { incorrectAnswer: false };
+      });
       this.props.showNextQuestionHandler();
+    } else {
+      this.setState(() => {
+        return { incorrectAnswer: true };
+      });
     }
   }
   render() {
@@ -28,6 +40,9 @@ class QuizQuestion extends Component {
             )}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? (
+          <p className="error">Sorry, that's not right</p>
+        ) : null}
       </main>
     );
   }
